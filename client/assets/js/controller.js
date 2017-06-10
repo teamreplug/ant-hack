e0329271af3940ad1beb96a3bc63aae194466c6e
Adeyemo Gabriel <adeyemo.gabriel@lmu.edu.ng>
2017-06-10 01:21:27 +0100

    Code Refactor
    Changes made

--- client/assets/js/controller.js
+++ client/assets/js/controller.js
@@ -1,9 +1,28 @@
 angular.module("antApp.controllers", [])
 
-.controller('loginController', function($scope, $rootScope){
+.controller('loginController', function($scope, $rootScope, $http){
+	$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+	$http.defaults.headers.common.Authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiJhbnQtaGFjayIsImlhdCI6MTQ5NzAzNTg0OX0.2YwkTDwSO9khGSvJNk1ZRdiZYdKkSo-DH7AnlnIjNh8';
+	$scope.login = function(user){
+		$http.post("/login", user).then(function(response){
+			if(response.data.status == "success"){
+				//move to proper route 
+
+			}else{
+				console.log(response.data.message)
+			}
+		}, function(error){
+			console.log(error.message)
+		})
+	}
 
 })
-.controller('noteController', function($scope, $rootScope){
+.controller('noteController', function($scope, $rootScope, $http){
+	$http.defaults.headers.common.Authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiJhbnQtaGFjayIsImlhdCI6MTQ5NzAzNTg0OX0.2YwkTDwSO9khGSvJNk1ZRdiZYdKkSo-DH7AnlnIjNh8';
+	
+	$scope.addNote = function(note){
+		
+	}
 
 })
 .controller('regController', function($scope, $rootScope){
